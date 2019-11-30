e5e2bd578679fc8b66d9705bb593a77aa42d4993
Kartik Mishra <kartikmishra04@gmail.com>
2019-11-30 22:16:26 +0530

    Adding Web view activity java file

---
+++ app/src/main/java/com/example/signzygithubapp/Activities/WebViewActivity.java
@@ -0,0 +1,33 @@
+package com.example.signzygithubapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.signzygithubapp.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        getSupportActionBar().hide();
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+}
